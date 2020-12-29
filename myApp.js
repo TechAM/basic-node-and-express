de2d6869397f3844bc2be4c73aac3b0cfb97c4a1
Avi Mukesh <avimukesh10@gmail.com>
2020-12-29 09:00:17 +0000

    Fixed bodyParser to parse POST requests

--- myApp.js
+++ myApp.js
@@ -10,11 +10,7 @@ app.use((req, res, next)=>{
 
 
 // --> 11)  Mount the body-parser middleware  here
-app.use((req, res, next)=>{
-	console.log(bodyParser)
-	bodyParser.urlencoded({extended:false})
-	next()
-})
+app.use(bodyParser.urlencoded({extended:false}))
 
 /** 1) Meet the node console. */
 console.log("Hello World");
